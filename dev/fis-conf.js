e61d33a4251c929623e9d59b59b40452177454f4
luojh <1303364524@qq.com>
2015-05-31 17:52:32 +0800

    a

---
+++ dev/fis-conf.js
@@ -0,0 +1,41 @@
+fis.config.set('pack', {
+    /*'pkg/lib.js': [
+        '/modules/underscore/**.js',
+        '/modules/backbone/**.js',
+        '/modules/jquery/**.js',
+        '/modules/vendor/**.js',
+        '/modules/common/**.js'
+    ]*/
+});
+
+//静态资源域名，使用pure release命令时，添加--domains或-D参数即可生效
+//fis.config.set('roadmap.domain', 'http://127.0.0.1:8080');
+
+//如果要兼容低版本ie显示透明png图片，请使用pngquant作为图片压缩器，
+//否则png图片透明部分在ie下会显示灰色背景
+//使用spmx release命令时，添加--optimize或-o参数即可生效
+//fis.config.set('settings.optimzier.png-compressor.type', 'pngquant');
+
+//设置jshint插件要排除检查的文件，默认不检查lib、jquery、backbone、underscore等文件
+//使用pure release命令时，添加--lint或-l参数即可生效
+fis.config.set('settings.lint.jshint.ignored', [ 'lib/**', /jquery|backbone|underscore/i ]);
+
+//csssprite处理时图片之间的边距，默认是3px
+fis.config.set('settings.spriter.csssprites.margin', 20);
+
+//后缀名的less的文件使用fis-parser-less编译
+//modules.parser.less表示设置后缀名为less的文件的parser，第二个less表示使用fis-parser-less进行编译
+fis.config.set('modules.parser.less', 'less');
+//将less文件编译为css
+fis.config.set('roadmap.ext.less', 'css');
+
+
+/*fis.config.set({
+    roadmap : {
+        domain : "http://127.0.0.1:88/ok/taobao/dist//static"
+    }
+});*/
+
+fis.config.set('roadmap', {
+        domain : "http://127.0.0.1:88/ok/taobao/dist/static"
+    });
